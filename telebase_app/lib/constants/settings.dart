b510a3cfb4a60f7bda0bf4c7d5fe3bfffd78c8fd
youm28 <opentoyou1218@gmail.com>
2025-12-22 22:03:40 +0900

    ipアドレス見やすく

--- telebase_app/lib/constants/settings.dart
+++ telebase_app/lib/constants/settings.dart
@@ -1,8 +1,7 @@
 const String ipAddress = String.fromEnvironment(
   "IP_ADDRESS",
   defaultValue: "10.40.42.28",
-  // カチャカのIPアドレス(研究室) 10.40.5.108
-  // カチャカのIPアドレス(H509) 10.40.42.28
+  // カチャカのIPアドレス
 );
 const int port = int.fromEnvironment("PORT", defaultValue: 26400);
 
